cb8e3f4677b91c58ee474c929e6da5e28c50501f
Reece Vela <101456455+reecevela@users.noreply.github.com>
2023-05-22 23:03:10 -0500

    Add bypass button for login

---
+++ chatbot/static/js/login.js
@@ -0,0 +1,8 @@
+// Bypasses login page for testing/viewing purposes
+const username = portfolio;
+const password = sitevisitor;
+
+document.getElementById("bypass").addEventListener("click", function() {
+    document.getElementById("id_username").value = username;
+    document.getElementById("id_password").value = password;
+});
--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 git restore .
 git pull
 
